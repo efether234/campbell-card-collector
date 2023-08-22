daf7b54d7872706bfd6353e79637fc67c03ee6a0
Evan Fetherolf <efether234@gmail.com>
2023-08-22 12:57:40 -0700

    Add GET '/cards' route.

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Card = require("../models/card");
 const router = express.Router();
 
-const getCards = require("../startup/download-cards");
-
-router.get("/", (req, res) => {
-  res.send("GET /cards");
-});
-
-router.get("/test", (req, res) => {
-  res.send("Check console");
+router.get("/", async (req, res) => {
+  const cards = await Card.find().select("-__v").sort("released_at");
+  res.send(cards);
 });
 
 module.exports = router;
